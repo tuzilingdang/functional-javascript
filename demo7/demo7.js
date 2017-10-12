b8eedd71e1326cdb33ab840731839f42e9b90f04
tuzilingdang <853651371@qq.com>
2017-10-12 10:54:31 +0800

    optimize recursion

--- demo7/demo7.js
+++ demo7/demo7.js
@@ -1,9 +1,16 @@
 
-function reduce(arr, fn, initial) {
-	if(arr.length) {
-		return reduce(arr.slice(1), fn, fn(initial, arr[0]));
-	}
-	else return initial;
+//function reduce(arr, fn, initial) {
+//	if(arr.length) {
+//		return reduce(arr.slice(1), fn, fn(initial, arr[0]));
+//	}
+//	else return initial;
+//}
+
+function reduce(arr, fn, initial){
+	return (function reduceOne(index, value){
+		if(index > arr.length - 1) return value;
+		return reduceOne(index + 1, fn(value, arr[index], index, arr), )
+	})(0, initial);
 }
 module.exports = reduce;
 
