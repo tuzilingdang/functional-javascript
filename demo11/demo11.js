52e33ae10c5f4fedf7e89d65dc4614ffaa2d2119
April <april@AprildeMacBook-Pro.local>
2017-10-21 18:30:09 +0800

    map with reduce

--- demo11/demo11.js
+++ demo11/demo11.js
@@ -1,1 +1,18 @@
 
+module.exports = function arrayMap(arr, fn, thisArg) {
+	console.log(thisArg);
+	return arr.reduce(function(acc, curr, index, arr) {
+		acc.push(fn(curr));
+		return acc;
+	}, [])
+}
+
+
+//var nums = [1,2,3,4,5]
+//
+//// `map` is your exported function
+//var output = arrayMap(nums, function double(item) {
+//  return item * 2
+//})
+
+//console.log(output) // => [2,4,6,8,10]])}))]
